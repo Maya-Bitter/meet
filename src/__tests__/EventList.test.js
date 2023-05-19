a1d08c60a85887639efdf86fd6a782dce8511235
Maya-Bitter <mayabitter@gmail.com>
2023-05-19 13:29:09 +0200

    .

--- src/__tests__/EventList.test.js
+++ src/__tests__/EventList.test.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { shallow } from "enzyme";
 import EventList from "../EventList";
-import Event from "./Event"; // in src/EventList.js
 import Event from "../Event"; // in src/__tests__/EventList.test.js
 
 describe("<EventList /> component", () => {
