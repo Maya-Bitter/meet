2bde9803604c9ab10fe235c4b4df9349eab13707
Maya-Bitter <mayabitter@gmail.com>
2023-05-19 13:23:16 +0200

    import Event

--- src/__tests__/EventList.test.js
+++ src/__tests__/EventList.test.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { shallow } from "enzyme";
 import EventList from "../EventList";
+import Event from "./Event"; // in src/EventList.js
+import Event from "../Event"; // in src/__tests__/EventList.test.js
 
 describe("<EventList /> component", () => {
   test("render correct number of events", () => {
