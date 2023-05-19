0e5fe009a65158455b74a3325ace1d3d2fd757dc
Maya-Bitter <mayabitter@gmail.com>
2023-05-19 10:50:06 +0200

    downgrade process

--- src/setupTests.js
+++ src/setupTests.js
@@ -8,4 +8,4 @@ Enzyme.configure({ adapter: new Adapter() });
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 
-import "@testing-library/jest-dom";
+// import "@testing-library/jest-dom";
