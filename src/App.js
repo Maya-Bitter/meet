5fbf99021a5517ee0f0ea204cabbb3cd8a1887cb
Maya-Bitter <mayabitter@gmail.com>
2023-05-25 10:09:19 +0200

    new css file added

--- src/App.js
+++ src/App.js
@@ -2,13 +2,27 @@ import React, { Component } from "react";
 import "./App.css";
 import EventList from "./EventList";
 import CitySearch from "./CitySearch";
-import { getEvents } from "./api";
+import NumberOfEvents from "./NumberOfEvents";
+import { getEvents, extractLocations } from "./api";
 
 class App extends Component {
   state = {
     events: [],
     locations: [],
   };
+
+  componentDidMount() {
+    this.mounted = true;
+    getEvents().then((events) => {
+      if (this.mounted) {
+        this.setState({ events, locations: extractLocations(events) });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.mounted = false;
+  }
 
   updateEvents = (location) => {
     getEvents().then((events) => {
@@ -30,6 +44,7 @@ class App extends Component {
           updateEvents={this.updateEvents}
         />
         <EventList events={this.state.events} />
+        <NumberOfEvents />
       </div>
     );
   }
