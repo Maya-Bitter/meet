4bd7c3610e92e22739032270d12ce4f0ac082256
Maya-Bitter <mayabitter@gmail.com>
2023-05-25 11:39:09 +0200

    tests

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -4,11 +4,13 @@ class CitySearch extends Component {
   state = {
     query: "",
     suggestions: [],
+    showSuggestions: undefined,
   };
 
   handleItemClicked = (suggestion) => {
     this.setState({
       query: suggestion,
+      showSuggestions: false,
     });
 
     this.props.updateEvents(suggestion);
@@ -27,8 +29,15 @@ class CitySearch extends Component {
           className="city"
           value={this.state.query}
           onChange={this.handleInputChanged}
+          onFocus={() => {
+            this.setState({ showSuggestions: true });
+          }}
         />
-        <ul className="suggestions">
+        <ul
+          className="suggestions"
+          style={this.state.showSuggestions ? {} : { display: "none" }}
+        >
+          {" "}
           {this.state.suggestions.map((suggestion) => (
             <li
               key={suggestion}
--- src/__tests__/CitySearch.test.js
+++ src/__tests__/CitySearch.test.js
@@ -13,8 +13,24 @@ describe("<CitySearch /> component", () => {
     );
   });
 
-  test("renders a list of suggestions", () => {
-    expect(CitySearchWrapper.find(".suggestions")).toHaveLength(1);
+  test("selecting CitySearch input reveals the suggestions list", () => {
+    CitySearchWrapper.find(".city").simulate("focus");
+    expect(CitySearchWrapper.state("showSuggestions")).toBe(true);
+    expect(CitySearchWrapper.find(".suggestions").prop("style")).not.toEqual({
+      display: "none",
+    });
+  });
+
+  test("selecting a suggestion should hide the suggestions list", () => {
+    CitySearchWrapper.setState({
+      query: "Berlin",
+      showSuggestions: undefined,
+    });
+    CitySearchWrapper.find(".suggestions li").at(0).simulate("click");
+    expect(CitySearchWrapper.state("showSuggestions")).toBe(false);
+    expect(CitySearchWrapper.find(".suggestions").prop("style")).toEqual({
+      display: "none",
+    });
   });
 
   test("renders text input correctly", () => {
@@ -43,6 +59,10 @@ describe("<CitySearch /> component", () => {
         suggestions[i]
       );
     }
+  });
+
+  test("renders a list of suggestions", () => {
+    expect(CitySearchWrapper.find(".suggestions")).toHaveLength(1);
   });
 
   test("selecting a suggestion should change query state", () => {
