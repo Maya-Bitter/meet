161b789b54ca236de504e48a00e72d7ba3cab56b
Maya-Bitter <mayabitter@gmail.com>
2023-06-06 13:41:59 +0200

    fixed code

--- src/App.js
+++ src/App.js
@@ -17,7 +17,10 @@ class App extends Component {
     this.mounted = true;
     getEvents().then((events) => {
       if (this.mounted) {
-        this.setState({ events, locations: extractLocations(events) });
+        this.setState({
+          events: events.slice(0, this.state.NumberOfEvents),
+          locations: extractLocations(events),
+        });
       }
     });
   }
