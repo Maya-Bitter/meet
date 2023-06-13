6958a7d269f6e2d6439342e0289c009cd94c7270
Maya-Bitter <mayabitter@gmail.com>
2023-06-13 10:48:22 +0200

    .

--- src/App.js
+++ src/App.js
@@ -94,6 +94,7 @@ class App extends Component {
 
         <div className="data-vis-wrapper">
           <EventGenre events={this.state.events} />
+
           <ResponsiveContainer height={400}>
             <ScatterChart margin={{ top: 20, right: 20, bottom: 20, left: 20 }}>
               <CartesianGrid />
